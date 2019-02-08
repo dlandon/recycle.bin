13e6d251cb7cb854c0f51faa290b31a4d0912ebf
dlandon <dlandon@cinci.rr.com>
2019-02-07 20:17:41 -0500

    Remove cr

--- source/RecycleBinAppend.php
+++
@@ -1,7 +0,0 @@
-Menu="Buttons:100"
-Link="nav-user"
-Markdown="false"
----
-<script type="text/javascript">
-  if (jQuery.inArray("RecycleBin",location.pathname.split('/'))!='-1'){rbit=$.cookie($.cookie('one')==null?'tab':'one');$(window).unload(function(){$.cookie('tab',rbit,{path:'/'});});}
-</script>
